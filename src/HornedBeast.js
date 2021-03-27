faa785c3adce642ad342244a438dbb91cad179d9
ALOYSIOUS <75811965+AL0YSI0US@users.noreply.github.com>
2021-03-27 12:13:41 -0700

    Update HornedBeast.js

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -19,14 +19,11 @@ buttonClicked = () => {
         <Card style={{ width: '18rem' }}
           onClick={this.buttonClicked}>
           <Card.Img variant="top" src={this.props.img} />
-            <Card.Body>
-               <Card.Title>{this.props.title}</Card.Title>
-                 <Card.Text>{this.props.description}
-                </Card.Text>
-              <Card.Text>
-                 ❤️ = {this.state.slapaaHeartOnIt}
-              </Card.Text>
-            </Card.Body>
+          <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Text>{this.props.description}</Card.Text>
+            <Card.Text>❤️ = {this.state.slapaaHeartOnIt}</Card.Text>
+          </Card.Body>
         </Card>
       </div>
     )
